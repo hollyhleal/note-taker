94908cff8ae842b56b9887f7f64e97b341f791ea
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-03-11 15:11:11 -0700

    add router for notes and update to server.js for testing

---
+++ routes/notes.js
@@ -0,0 +1,32 @@
+const notes = require("express").Router();
+
+// GET /api/notes should read the db.json file and return all saved notes as JSON
+notes.get("/", (req, res) =>
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)))
+);
+
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. Find a way to give each note a unique id when it's saved.
+notes.post("/", (req, res) => {
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      //note_id:
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json("Error in posting new note.");
+  }
+});
+
+module.exports = notes;
